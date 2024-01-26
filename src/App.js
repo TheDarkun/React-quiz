b441ae051bf3265824ac7dd4177209837132cd76
Darkun <106868917+TheDarkun@users.noreply.github.com>
2024-01-26 21:49:01 +0100

    add useReducer; add loading, error and start screens

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import Header from "./Header";
 import Main from "./Main";
-import {useEffect} from "react";
+import {useEffect, useReducer} from "react";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+
+const initialState = {
+    questions: [],
+    status: "loading"
+}
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "dataReceived":
+            return {
+                ...state,
+                questions: action.payload,
+                status: "ready"
+            }
+        case "dataFailed":
+            return {
+                ...state,
+                status: "error"
+            }
+        default:
+            throw new Error("Action unknown");
+    }
+}
+
 
 export default function App() {
-    
+
+    const [{questions, status}, dispatch] = useReducer(reducer, initialState);
+
     useEffect(() => {
         fetch("http://localhost:8000/questions")
             .then(result => result.json())
-            .then(data => console.log(data))
-            .catch(error => console.log(error))
+            .then(data => dispatch({type: "dataReceived", payload: data}))
+            .catch(error => dispatch({type: "dataFailed"}))
     }, [])
     return (
         <div className="app">
             <Header/>
             <Main>
-                <p>1/15</p>
-                <p>Question?</p>
+                {status === "loading" && <Loader/>}
+                {status === "error" && <Error/>}
+                {status === "ready" && <StartScreen numQuestion={questions.length}/>}
             </Main>
         </div>
     );
 }
-
-
