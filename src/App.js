1cb0dad56925d8d5631689d6403288a07462f3c2
Darkun <106868917+TheDarkun@users.noreply.github.com>
2024-01-26 18:49:05 +0100

    add json server

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import DateCounter from "./DateCounter";
+import Header from "./Header";
+import Main from "./Main";
+import {useEffect} from "react";
 
 export default function App() {
-  return (
-    <div className="App">
-      <DateCounter/>
-    </div>
-  );
+    
+    useEffect(() => {
+        fetch("http://localhost:8000/questions")
+            .then(result => result.json())
+            .then(data => console.log(data))
+            .catch(error => console.log(error))
+    }, [])
+    return (
+        <div className="app">
+            <Header/>
+            <Main>
+                <p>1/15</p>
+                <p>Question?</p>
+            </Main>
+        </div>
+    );
 }
 
 
